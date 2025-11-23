966f5c96ef9613324a722bd78c7d7dbe069a4a62
Gabriel Adryan <gabriel.pinto@sptech.school>
2025-11-23 00:22:05 -0300

    att

--- Repositório-Técnico/public/src/controllers/usuarioController.js
+++ Repositório-Técnico/public/src/controllers/usuarioController.js
@@ -25,20 +25,6 @@ function autenticar(req, res) {
     nome: resultadoAutenticar[0].nome
     
 });
-                    //     aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                    //         .then((resultadoAquarios) => {
-                    //             if (resultadoAquarios.length > 0) {
-                    //                 res.json({
-                    //                     id_usuario: resultadoAutenticar[0].id,
-                    //                     email: resultadoAutenticar[0].email,
-                    //                     nome: resultadoAutenticar[0].nome,
-                    //                     senha: resultadoAutenticar[0].senha,
-                    //                     aquarios: resultadoAquarios
-                    //                 });
-                    //             } else {
-                    //                 res.status(204).json({ aquarios: [] });
-                    //             }
-                    //         })
 }
 else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
@@ -58,7 +44,6 @@ else if (resultadoAutenticar.length == 0) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
